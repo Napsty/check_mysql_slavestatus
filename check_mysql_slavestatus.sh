ed57dfb59ae2d44d67dd4ce7d04f1262723b911a
Napsty <ck@claudiokuenzler.com>
2013-10-16 16:49:30 +0200

    New threshold handling, help rewrite, optical makeover

--- check_mysql_slavestatus.sh
+++ check_mysql_slavestatus.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 #########################################################################
-# Script:	check_mysql_slavestatus.sh				#
-# Author:	Claudio Kuenzler www.claudiokuenzler.com		#
-# Purpose:	Monitor MySQL Replication status with Nagios		#
-# Description:	Connects to given MySQL hosts and checks for running 	#
-#		SLAVE state and delivers additional info		#
-# Original:	This script is a modified version of 			#
-#		check mysql slave sql running written by dhirajt	#
-# Thanks to:	Victor Balada Diaz for his ideas added on 20080930	#
-#		Soren Klintrup for stuff added on 20081015		#
-#		Marc Feret for Slave_IO_Running check 20111227		#
-#		Peter Lecki for his mods added on 20120803		#
-# History:
-# 2008041700 Original Script modified					#
-# 2008041701 Added additional info if status OK				#
-# 2008041702 Added usage of script with params -H -u -p			#
-# 2008041703 Added bindir variable for multiple platforms		#
-# 2008041704 Added help because mankind needs help			#
-# 2008093000 Using /bin/sh instead of /bin/bash				#
-# 2008093001 Added port for MySQL server				#
-# 2008093002 Added mysqldir if mysql binary is elsewhere		#
-# 2008101501 Changed bindir/mysqldir to use PATH			#
-# 2008101501 Use $() instead of `` to avoid forks			#
-# 2008101501 Use ${} for variables to prevent problems			#
-# 2008101501 Check if required commands exist				#
-# 2008101501 Check if mysql connection works				#
-# 2008101501 Exit with unknown status at script end			#
-# 2008101501 Also display help if no option is given			#
-# 2008101501 Add warning/critical check to delay			#
-# 2011062200 Add perfdata						#
-# 2011122700 Checking Slave_IO_Running					#
-# 2012080300 Changed to use only one mysql query			#
-# 2012080301 Added warn and crit delay as optional args			#
-# 2012080302 Added standard -h option for syntax help			#
-# 2012080303 Added check for mandatory options passed in		#
-# 2012080304 Added error output from mysql				#
-# 2012080305 Changed from 'cut' to 'awk' (eliminate ws)			#
-# 2012111600 Do not show password in error output			#
-# 2013042800 Changed PATH to use existing PATH, too			#
-# 2013050800 Bugfix in PATH export					#
-# 2013092700 Bugfix in PATH export					#
-# 2013092701 Bugfix in getopts						#
+# Script:	check_mysql_slavestatus.sh                              #
+# Author:	Claudio Kuenzler www.claudiokuenzler.com                #
+# Purpose:	Monitor MySQL Replication status with Nagios            #
+# Description:	Connects to given MySQL hosts and checks for running    #
+#		SLAVE state and delivers additional info                #
+# Original:	This script is a modified version of                    #
+#		check mysql slave sql running written by dhirajt        #
+# Thanks to:	Victor Balada Diaz for his ideas added on 20080930      #
+#		Soren Klintrup for stuff added on 20081015              #
+#		Marc Feret for Slave_IO_Running check 20111227          #
+#		Peter Lecki for his mods added on 20120803              #
+# History:                                                              #
+# 2008041700 Original Script modified                                   #
+# 2008041701 Added additional info if status OK	                        #
+# 2008041702 Added usage of script with params -H -u -p	                #
+# 2008041703 Added bindir variable for multiple platforms               #
+# 2008041704 Added help because mankind needs help                      #
+# 2008093000 Using /bin/sh instead of /bin/bash                         #
+# 2008093001 Added port for MySQL server                                #
+# 2008093002 Added mysqldir if mysql binary is elsewhere                #
+# 2008101501 Changed bindir/mysqldir to use PATH                        #
+# 2008101501 Use $() instead of `` to avoid forks                       #
+# 2008101501 Use ${} for variables to prevent problems                  #
+# 2008101501 Check if required commands exist                           #
+# 2008101501 Check if mysql connection works                            #
+# 2008101501 Exit with unknown status at script end                     #
+# 2008101501 Also display help if no option is given                    #
+# 2008101501 Add warning/critical check to delay                        #
+# 2011062200 Add perfdata                                               #
+# 2011122700 Checking Slave_IO_Running                                  #
+# 2012080300 Changed to use only one mysql query                        #
+# 2012080301 Added warn and crit delay as optional args                 #
+# 2012080302 Added standard -h option for syntax help                   #
+# 2012080303 Added check for mandatory options passed in                #
+# 2012080304 Added error output from mysql                              #
+# 2012080305 Changed from 'cut' to 'awk' (eliminate ws)                 #
+# 2012111600 Do not show password in error output                       #
+# 2013042800 Changed PATH to use existing PATH, too                     #
+# 2013050800 Bugfix in PATH export                                      #
+# 2013092700 Bugfix in PATH export                                      #
+# 2013092701 Bugfix in getopts                                          #
+# 2013101600 Rewrite of threshold logic and handling                    #
+# 2013101601 Optical clean up                                           #
+# 2013101602 Rewrite help output                                        #
 #########################################################################
-# Usage: ./check_mysql_slavestatus.sh -H dbhost -P port -u dbuser -p dbpass -w warn_delay -c crit_delay
+# Usage: ./check_mysql_slavestatus.sh -H dbhost -P port -u dbuser -p dbpass -w integer -c integer
 #########################################################################
 help="\ncheck_mysql_slavestatus.sh (c) 2008-2013 GNU GPLv2 licence
-Usage: check_mysql_slavestatus.sh -H host -P port -u username -p password [-w warn_delay] [-c crit_delay]\n
-Options:\n-H Hostname or IP of slave server\n-P Port of slave server\n-u Username of DB-user\n-p Password of DB-user\n-w Delay in seconds for Warning status (optional, default ${warn_delay})\n-c Delay in seconds for Critical status (optional, default ${crit_delay})\n
-Attention: The DB-user you type in must have CLIENT REPLICATION rights on the DB-server.\n"
+Usage: check_mysql_slavestatus.sh -H host -P port -u username -p password [-w integer] [-c integer]\n
+Options:\n-H Hostname or IP of slave server\n-P Port of slave server\n-u Username of DB-user\n-p Password of DB-user\n-w Delay in seconds for Warning status (optional)\n-c Delay in seconds for Critical status (optional)\n
+Attention: The DB-user you type in must have CLIENT REPLICATION rights on the DB-server. Example:\n\tGRANT REPLICATION CLIENT on *.* TO 'nagios'@'%' IDENTIFIED BY 'secret';"
 
 STATE_OK=0		# define the exit code if status is OK
 STATE_WARNING=1		# define the exit code if status is Warning (not really used)
@@ -113,35 +116,35 @@ delayinfo=`echo "${ConnectionResult}" |grep Seconds_Behind_Master | awk '{print 
 # Output of different exit states
 #########################################################################
 if [ ${check} = "NULL" ]; then 
-echo CRITICAL: Slave_SQL_Running is answering NULL
-exit ${STATE_CRITICAL};
+echo "CRITICAL: Slave_SQL_Running is answering NULL"; exit ${STATE_CRITICAL};
 fi
 
 if [ ${check} = ${crit} ]; then 
-echo CRITICAL: ${host}:${port} Slave_SQL_Running: ${check}
-exit ${STATE_CRITICAL};
+echo "CRITICAL: ${host}:${port} Slave_SQL_Running: ${check}"; exit ${STATE_CRITICAL};
 fi
 
 if [ ${checkio} = ${crit} ]; then 
-echo CRITICAL: ${host} Slave IO Running: ${checkio}
-exit ${STATE_CRITICAL};
+echo "CRITICAL: ${host} Slave IO Running: ${checkio}"; exit ${STATE_CRITICAL};
 fi
 
 if [ ${check} = ${ok} ]; then
- if [ "${delayinfo}" -ge "${warn_delay}" ]
- then
-  if [ "${delayinfo}" -ge "${crit_delay}" ]
-  then
-   echo "CRITICAL: slave is ${delayinfo} seconds behind master | delay=${delayinfo}s"
-   exit ${STATE_CRITICAL}
-  else
-   echo "WARNING: slave is ${delayinfo} seconds behind master | delay=${delayinfo}s"
-   exit ${STATE_WARNING}
+# Delay thresholds are set
+if [[ -n ${warn_delay} ]] && [[ -n ${crit_delay} ]]; then
+  if ! [[ ${warn_delay} -gt 0 ]]; then echo "Warning threshold must be a valid integer greater than 0"; exit $STATE_UNKNOWN; fi
+  if ! [[ ${crit_delay} -gt 0 ]]; then echo "Warning threshold must be a valid integer greater than 0"; exit $STATE_UNKNOWN; fi
+  if [[ -z ${warn_delay} ]] || [[ -z ${crit_delay} ]]; then echo "Both warning and critical thresholds must be set"; exit $STATE_UNKNOWN; fi
+  if [[ ${warn_delay} -gt ${crit_delay} ]]; then echo "Warning threshold cannot be greater than critical"; exit $STATE_UNKNOWN; fi
+
+  if [[ ${delayinfo} -ge ${crit_delay} ]]
+  then echo "CRITICAL: Slave is ${delayinfo} seconds behind Master | delay=${delayinfo}s"; exit ${STATE_CRITICAL}
+  elif [[ ${delayinfo} -ge ${warn_delay} ]]
+  then echo "WARNING: Slave is ${delayinfo} seconds behind Master | delay=${delayinfo}s"; exit ${STATE_WARNING}
+  else echo "OK: Slave SQL running: ${check} Slave IO running: ${checkio} / master: ${masterinfo} / slave is ${delayinfo} seconds behind master | delay=${delayinfo}s"; exit ${STATE_OK};
   fi
- else
-  echo "OK: Slave SQL running: ${check} Slave IO running: ${checkio} / master: ${masterinfo} / slave is ${delayinfo} seconds behind master | delay=${delayinfo}s"
-  exit ${STATE_OK};
- fi
+fi
+# Without delay thresholds
+echo "OK: Slave SQL running: ${check} Slave IO running: ${checkio} / master: ${masterinfo} / slave is ${delayinfo} seconds behind master | delay=${delayinfo}s"
+exit ${STATE_OK};
 fi
 
 echo "UNKNOWN: should never reach this part"
