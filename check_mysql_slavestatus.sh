025a5cbd24dccd7a2f00dc8420eca5bb06e4528d
Marc Falzon <marc@falzon.me>
2014-10-09 15:03:34 +0200

    Support for option file
    This change provides support for passing a path to a MySQL "option file"
    with the option `-o`, thus passing a host/port/user/password is now
    optional.

--- check_mysql_slavestatus.sh
+++ check_mysql_slavestatus.sh
@@ -54,8 +54,8 @@
 # Usage: ./check_mysql_slavestatus.sh -H dbhost -P port -u dbuser -p dbpass -s connection -w integer -c integer
 #########################################################################
 help="\ncheck_mysql_slavestatus.sh (c) 2008-2014 GNU GPLv2 licence
-Usage: check_mysql_slavestatus.sh -H host -P port -u username -p password [-s connection] [-w integer] [-c integer]\n
-Options:\n-H Hostname or IP of slave server\n-P Port of slave server\n-u Username of DB-user\n-p Password of DB-user\n-s Connection name (optional, with multi-source replication)\n-w Delay in seconds for Warning status (optional)\n-c Delay in seconds for Critical status (optional)\n
+Usage: check_mysql_slavestatus.sh [-o option_file] [-H host] [-P port] [-u username] [-p password] [-s connection] [-w integer] [-c integer]\n
+Options:\n-o Path to option file (e.g. /root/.my.cnf)\n-H Hostname or IP of slave server\n-P Port of slave server\n-u Username of DB-user\n-p Password of DB-user\n-s Connection name (optional, with multi-source replication)\n-w Delay in seconds for Warning status (optional)\n-c Delay in seconds for Critical status (optional)\n
 Attention: The DB-user you type in must have CLIENT REPLICATION rights on the DB-server. Example:\n\tGRANT REPLICATION CLIENT on *.* TO 'nagios'@'%' IDENTIFIED BY 'secret';"
 
 STATE_OK=0		# define the exit code if status is OK
@@ -85,18 +85,20 @@ fi
 
 # Important given variables for the DB-Connect
 #########################################################################
-while getopts "H:P:u:p:s:w:c:h" Input;
+while getopts "H:P:u:p:s:w:c:o:h" Input;
 do
 	case ${Input} in
-	H)	host=${OPTARG};;
-	P)	port=${OPTARG};;
-	u)	user=${OPTARG};;
-	p)	password=${OPTARG};;
+	H)	host="-h ${OPTARG}";;
+	P)	port="-P ${OPTARG}";;
+	S)	socket="-S ${OPTARG}";;
+	u)	user="-u ${OPTARG}";;
+	p)	password="-p ${OPTARG}";;
 	s)	connection=\"${OPTARG}\";;
 	w)      warn_delay=${OPTARG};;
 	c)      crit_delay=${OPTARG};;
+	o)      optfile="--defaults-extra-file=${OPTARG}";;
 	h)      echo -e "${help}"; exit 1;;
-	\?)	echo "Wrong option given. Please use options -H for host, -P for port, -u for user and -p for password"
+	\?)	echo "Wrong option given."
 		exit 1
 		;;
 	esac
@@ -104,15 +106,10 @@ done
 
 # Connect to the DB server and check for informations
 #########################################################################
-# Check whether all required arguments were passed in
-if [ -z "${host}" -o -z "${port}" -o -z "${user}" -o -z "${password}" ];then
-	echo -e "${help}"
-	exit ${STATE_UNKNOWN}
-fi
 # Connect to the DB server and store output in vars
-ConnectionResult=`mysql -h ${host} -P ${port} -u ${user} --password=${password} -e "show slave ${connection} status\G" 2>&1`
+ConnectionResult=`mysql ${optfile} ${host} ${port} ${user} ${password} -e "show slave ${connection} status\G" 2>&1`
 if [ -z "`echo "${ConnectionResult}" |grep Slave_IO_State`" ]; then
-	echo -e "CRITICAL: Unable to connect to server ${host}:${port} with username '${user}' and given password"
+	echo -e "CRITICAL: Unable to connect to server"
 	exit ${STATE_CRITICAL}
 fi
 check=`echo "${ConnectionResult}" |grep Slave_SQL_Running: | awk '{print $2}'`
