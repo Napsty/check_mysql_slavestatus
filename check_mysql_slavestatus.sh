3a8b334afeb8f4b35e1598a9080f3181135bf072
Claudio Kuenzler <ck@claudiokuenzler.com>
2023-06-02 07:50:48 +0200

    Update documentation for privileges MariaDB 10.5+ (#18)

--- check_mysql_slavestatus.sh
+++ check_mysql_slavestatus.sh
@@ -62,13 +62,15 @@
 # 2019082204 Fix moving subcheck, improve documentation                 #
 # 2020093000 Added multi-channel replication (-C)                       #
 # 2023010300 Detect either Host or Socket argument (issue #14)          #
+# 2023060200 Update documentation for privileges MariaDB 10.5+          #
 #########################################################################
 # Usage: ./check_mysql_slavestatus.sh (-o file|(-H dbhost [-P port]|-S socket) -u dbuser -p dbpass) ([-s connection]|[-C channel]) [-w integer] [-c integer] [-m integer]
 #########################################################################
 help="\ncheck_mysql_slavestatus.sh (c) 2008-2023 GNU GPLv2 licence
 Usage: $0 (-o file|(-H dbhost [-P port]|-S socket) -u username -p password) ([-s connection]|[-C channel]) [-w integer] [-c integer] [-m]\n
 Options:\n-o Path to option file containing connection settings (e.g. /home/nagios/.my.cnf). Note: If this option is used, -H, -u, -p parameters will become optional\n-H Hostname or IP of slave server\n-P MySQL Port of slave server (optional, defaults to 3306)\n-u Username of DB-user\n-p Password of DB-user\n-S database socket\n-s Connection name (optional, with multi-source replication for mariadb)\n-C Channel (optional, with multi-source replication for mysql)\n-w Replication delay in seconds for Warning status (optional)\n-c Replication delay in seconds for Critical status (optional)\n-m Threshold in seconds since when replication did not move (compares the slaves log position)\n
-Attention: The DB-user you type in must have CLIENT REPLICATION rights on the DB-server. Example:\n\tGRANT REPLICATION CLIENT on *.* TO 'nagios'@'%' IDENTIFIED BY 'secret';"
+Attention: The DB-user you type in must have CLIENT REPLICATION rights on the DB-server. Example:\n\tGRANT REPLICATION CLIENT on *.* TO 'nagios'@'monitoringhost' IDENTIFIED BY 'secret';\n
+If you use MariaDB 10.5 or newer, the DB user must have REPLICA MONITOR rights:\n\tGRANT REPLICA MONITOR ON *.* TO 'nagios'@'monitoringhost' IDENTIFIED BY 'secret';\n"
 
 STATE_OK=0              # define the exit code if status is OK
 STATE_WARNING=1         # define the exit code if status is Warning (not really used)
